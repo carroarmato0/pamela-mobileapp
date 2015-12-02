6a9604bf280d21772ced41e428c470b879b6b592
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-12-02 23:10:10 +0100

    Initial commit
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var PAMELA_DATA_URL = 'https://pamela.inuits.eu/macs.php'
+
+// AJAX CALL to the DATA URL
+$.ajax({
+	url: PAMELA_DATA_URL,
+	context: document.body,
+	datatype: 'script',
+}).done(function(data) {
+
+	var jsonArray = JSON.parse("[" + data + "]");
+	var array = $.map(jsonArray, function(el) { return el });
+
+	$('ul#presence_list').empty();
+
+	$.each(array, function(index, value) {
+		$('ul#presence_list').append("<li>" + value + "</li>");
+	});
+});
