2a9ca33d3bb6a6b6ddc449fe08aac7478b49875f
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-12-09 16:46:34 +0100

    Sort response
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ var PAMELA_DATA_URL = 'https://pamela.inuits.eu/macs.php'
 
 // AJAX CALL to the DATA URL
 $.ajax({
-	url: PAMELA_DATA_URL,
-	context: document.body,
-	datatype: 'script',
+  url: PAMELA_DATA_URL,
+  context: document.body,
+  datatype: 'script',
 }).done(function(data) {
 
-	var jsonArray = JSON.parse("[" + data + "]");
-	var array = $.map(jsonArray, function(el) { return el });
+  var jsonArray = JSON.parse("[" + data + "]");
+  var unsorted_array = $.map(jsonArray, function(el) { return el });
+  var array = unsorted_array.sort();
 
-	$('ul#presence_list').empty();
+  $('ul#presence_list').empty();
 
-	$.each(array, function(index, value) {
-		$('ul#presence_list').append("<li>" + value + "</li>");
-	});
+  $.each(array, function(index, value) {
+    $('ul#presence_list').append("<li>" + value + "</li>");
+  });
 
-	$('#filter').fastLiveFilter('#presence_list');
+  $('#filter').fastLiveFilter('#presence_list');
 });
