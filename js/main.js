b0b08af1f403dd2ad9528b9a55fea85ace347103
root <root@pamela.internal.inuits.eu>
2016-03-15 11:39:10 +0100

    Sorted users by office

--- js/main.js
+++ js/main.js
@@ -14,8 +14,42 @@ $.ajax({
   $('ul#presence_list').empty();
 
   $.each(array, function(index, value) {
-    $('ul#presence_list').append("<li>" + value + "</li>");
-  });
+	
+	var str = array[index];
 
-  $('#filter').fastLiveFilter('#presence_list');
+	if (str.toLowerCase().indexOf("brasschaat") >= 0){
+    		$('ul#presence_list_brasschaat').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}
+	else if (str.toLowerCase().indexOf("gent") >= 0){
+		$('ul#presence_list_gent').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}  
+	else if (str.toLowerCase().indexOf("prague") >= 0){
+    		$('ul#presence_list_prague').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}  
+	else if (str.toLowerCase().indexOf("kyiv") >= 0){
+    		$('ul#presence_list_kyiv').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}  
+	else if (str.toLowerCase().indexOf("rotterdam") >= 0){
+    		$('ul#presence_list_rotterdam').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}  
+	else if (str.toLowerCase().indexOf("brno") >= 0){
+    		$('ul#presence_list_brno').append("<li>" + value.substr(0, value.indexOf('@')) + "</li>");
+	}  
+	else {
+    		$('ul#presence_list_other').append("<li>" + value + "</li>");
+	}  
+
+  
+});
+
+  $('#filter').fastLiveFilter('#presence_list_brasschaat');
+  $('#filter').fastLiveFilter('#presence_list_prague');
+  $('#filter').fastLiveFilter('#presence_list_gent');
+  $('#filter').fastLiveFilter('#presence_list_rotterdam');
+  $('#filter').fastLiveFilter('#presence_list_brno');
+  $('#filter').fastLiveFilter('#presence_list_kyiv');
+  $('#filter').fastLiveFilter('#presence_list_other');
+
+  $('.townheader:has(li)').show();
+
 });
